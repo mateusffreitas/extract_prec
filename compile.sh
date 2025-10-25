4c5923ff15132c82d33a89520ef615c689e627d0
Mateus FFreitas <mateus.ffreitas@hotmail.com>
2025-10-25 00:00:04 -0300

    Adiciona script de compilação

---
+++ compile.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PREREQ_DIR=${HOME}/opt-gcc
+
+export PATH=${PREREQ_DIR}/bin:$PATH
+export LD_LIBRARY_PATH=${PREREQ_DIR}/lib:$LD_LIBRARY_PATH
+
+gfortran -O3 -o extract_prec mod_out.f90 mod_netcdf.f90  mod_polygon_point_chek.f90 \
+   extract_prec.f90 -I${PREREQ_DIR}/include -L${PREREQ_DIR}/lib -lnetcdff -lnetcdf -ffree-form -ffree-line-length-none
